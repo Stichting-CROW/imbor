2086d3818ac82cbbb20350669d9d0a5508e93fed
Redmer Kronemeijer <kronemeijer@rdmr.eu>
2021-01-06 22:53:36 +0100

    ReSpec opgesplits uit index.html

---
+++ imbor-ld/config.js
@@ -0,0 +1,22 @@
+var respecConfig = {
+    specStatus: "CROW-BASIS",
+    format: "markdown",
+    github: "https://github.com/Stichting-CROW/imbor",
+    pubDomain: "imbor",
+    shortName: "imbor-ld",
+    lint: {
+        "no-http-props": false
+    },
+    editors: [
+        {
+            name: "Rik",
+            company: "CROW",
+            note: "in opdracht van BIM Pro"
+        },
+        {
+            name: "Redmer Kronemeijer",
+            company: "CROW",
+            note: "in opdracht van BIM Pro"
+        }
+    ],
+};
---
+++ imbor-ld/docs/abstract.md
@@ -0,0 +1,1 @@
+Deze documentatie beschrijft de onderdelen van de LinkedData versie van IMBOR (IMBOR-LD).
