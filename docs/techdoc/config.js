419a51beb2886a023e532e605bac4a4cb3c8292b
RiX012 <56293445+RiX012@users.noreply.github.com>
2022-02-22 15:36:16 +0100

    tyi

--- docs/techdoc/config.js
+++ docs/techdoc/config.js
@@ -2,7 +2,7 @@ var respecConfig = { // => https://github.com/stichting-crow/respec/wiki
     specType: "CROW-DOC",
     specStatus: "WD",
     pubDomain: "imbor-techdoc",
-    shortName: "imbor-techdoc",
+    shortName: "techdoc",
     prevRecURI: "https://docs.crow.nl/imbor/techdoc/cv-20211101/",
     editors: [
         {
