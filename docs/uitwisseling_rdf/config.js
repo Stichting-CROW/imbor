5171f94d26e225efa6d966eef186c2681492b5eb
RiX012 <56293445+RiX012@users.noreply.github.com>
2022-06-03 16:05:46 +0200

    update geodata

---
+++ docs/uitwisseling_rdf/config.js
@@ -0,0 +1,14 @@
+var respecConfig = {
+  // => https://github.com/stichting-crow/respec/wiki
+  specType: "Best practice", // vrije tekst
+  specStatus: "DRAFT", // kies uit https://github.com/Stichting-CROW/respec/wiki/specStatus
+  imprint: "crow", // Kies uit http://docs.crow.nl/respec-design/stichting-crow/#logos
+  shortName: "imbor/uitwisseling_rdf",
+  license: "CC-BY", 
+  editors: [
+    {
+      name: "Rik | CROW", // verplicht, de rest is optioneel
+    },
+  ],
+  github: "https://github.com/Stichting-CROW/imbor",
+};
