b04fc6553f3253fe3a9b81e24fa1be36c669d0ab
Redmer Kronemeijer <redmer.kronemeijer@crow.nl>
2025-07-02 15:09:08 +0200

    Update config.js
    Volgens mij is dit het, ik heb het nog niet getest

--- docs/beheerplan/config.js
+++ docs/beheerplan/config.js
@@ -1,20 +1,23 @@
-var respecConfig = { // => https://github.com/stichting-crow/respec/wiki
-    specType: "CROW-DOC",
-    specStatus: "def",
-    pubDomain: "IMBOR",
-    shortName: "imbor/beheerplan",
-    previousPublishDate: "2022-01-01",
-    previousStatus: "def",
-    // prevVersion: "0.9",
-    editors: [
-        {
-            name: "Harro Verhoeven",
-            company: "CROW",
-        },
-        {
-            name: "Elisabeth de Vries",
-            company: "CROW",
-        }
-    ],
-    github: "https://github.com/Stichting-CROW/imbor",
+var respecConfig = {
+  // => https://github.com/stichting-crow/respec/wiki
+  specStatus: "DOC",
+  pubDomain: "imbor",
+  shortName: "beheerplan",
+  license: "CC-BY",
+  subtitle: "Handleiding voor beheerder informatiemodel",
+  editors: [
+    {
+        name: "Harro Verhoeven",
+        company: "CROW",
+    },
+    {
+        name: "Elisabeth de Vries",
+        company: "CROW",
+    }
+  ],
+  github: "https://github.com/Stichting-CROW/imbor",
+  hypothesisComments: false,
+  lint: { "no-unused-dfns": false },
+  localBiblio: window.localBibliography,
+  prevVersion: "https://docs.crow.nl/imbor/beheerplan-CROW-DOC-def-20220101.html"
 };
